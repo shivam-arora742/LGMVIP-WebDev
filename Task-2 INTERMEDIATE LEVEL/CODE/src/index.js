7347d834b14f4bb95a6a7aeb2436b213a414b303
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-09-21 22:42:37 +0530

    Delete Task-2 INTERMEDIATE LEVEL/CODE directory

--- Task-2 INTERMEDIATE LEVEL/CODE/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-// import Button from 'react-bootstrap/Button';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
